469b19261b45d2d6be79e06d3ba1d847ed40adf5
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-02-21 17:32:37 +0300

    some test passed

--- src/index.js
+++ src/index.js
@@ -1,31 +1,77 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.setCom = false;
+    this.f;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    var new_array = [];
+    for (var i = 0; i < indices.length; i++){
+      new_array.push(this.array[indices[i]]);
+    }
+    if (this.setCom == false){
+      for (var i = 0; i < new_array.length; i++){
+        for (var j = 0; j < new_array.length - i; j++) {
+          if (new_array[j] > new_array[j + 1]) {
+            var temp = new_array[j];
+            new_array[j] = new_array[j + 1];
+            new_array[j + 1] = temp;
+          }
+        }
+      }
+    } else {
+      new_array = step(new_array, this.f);
+    }
+    for (var i = 0; i < indices.length; i++) {
+      this.array[indices[i]] = new_array[i];
+    }
+    return this.array;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.setCom == true;
+    this.f = compareFunction;
   }
 }
 
+function step(par, f) {
+  for (var i = 0; i < par.length; i++)
+    if (f(par[i], par[i + 1]) > 0) {
+      var x = par[i];
+      par[i] = par[i + 1];
+      par[i + 1] = x;
+    } else {
+      continue;
+    }
+  return par;
+}
+
 module.exports = Sorter;
+
+var sorter = new Sorter;
+sorter.add(6);
+sorter.add(5);
+sorter.sort([0, 1]);
+
+sorter.add(2);
+sorter.add(1);
+sorter.sort([1, 0]);
+
+sorter.toArray();
